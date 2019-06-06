fd8fdd423aa88c3215d6448f0c7aa3c6f359c37a
FateScript <422643055@qq.com>
2019-06-06 15:47:57 +0800

    add flake8 config

--- autoConfig.sh
+++ autoConfig.sh
@@ -2,6 +2,7 @@
 cp light_vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+mv flake8 ~/.flake8
 
 #### YCM config
 cd ~/.vim/bundle/YouCompleteMe
@@ -10,6 +11,7 @@ sudo apt install build-essential cmake python3-dev
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --clang-completer
 cp ycm_extra_conf.py  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp/ycm/.ycm_extra_conf.py
+pip3 install --user flake8
 
 ### zsh config
 sudo apt update
