ec81f353f2dd8e8eb703fb1a7f2bfcfb32fe2d91
FateScript <wangfeng02@megvii.com>
2022-03-10 17:10:17 +0800

    chore(zsh): update vim and zsh

---
+++ zsh/alias.zsh
@@ -0,0 +1,52 @@
+# aliases {
+alias -g L='| less'
+alias -g JL='| jq | less'
+alias -g CL='| pygmentize | less'
+alias -g G='| grep'
+
+alias m='make'
+
+alias sv='sudo vim'
+alias vi='vim'
+alias v=vim
+alias c='cat'
+
+alias ipy="python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
+
+alias rf='readlink -f'
+
+alias findname='find . -name'
+
+# kill process with python, run `pshow python KILL`
+alias pshow="ps -ef | grep"
+alias -g KILL="| awk '{print \$2}' | head -n -1 | xargs kill -9"
+
+# diff dir a and b, run `diffdir a b DIFF`
+alias diffdir="diff --exclude '*.txt' --exclude '*.pkl' --exclude '*__pycache__*'"
+alias -g DIFF="--width=$COLUMNS --suppress-common-lines --side-by-side --recursive"
+
+# tar aliases
+alias tarinfo="tar -tf"
+alias tarzip="tar -zcvf"
+alias tarunzip="tar -zxvf"
+
+# git aliases
+alias gs='git status'
+alias ga='git add'
+alias gm='git commit -m'
+alias gd='git diff'
+alias grm='git rm'
+alias gmv='git mv'
+alias gck='git checkout'
+alias gr="git remote"
+alias gdown='git reset HEAD'
+alias gremove='git checkout --'
+
+if [ $OS_DISTRIBUTION = 'arch' ]; then
+	alias yS='yaourt -S --noconfirm --needed'
+	alias ySs='yaourt -Ss'
+	alias pS='sudo pacman -S --noconfirm --needed'
+	alias pSs='sudo pacman -Ss'
+fi
+
+# }
--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -10,14 +10,51 @@ get_os_distribution() {
 
 OS_DISTRIBUTION=$(get_os_distribution)
 
+# Developement Settings {
 act_venv() {
-    source ~/env/$1/bin/activate
+    source ~/workspace/env/$1/bin/activate
 }
 
+# auto set venv
 tmux_venv() {
     tmux set-env VIRTUAL_ENV $VIRTUAL_ENV
 }
 
+# auto unset venv
 tmux_rm_venv() {
     tmux set-env -r VIRTUAL_ENV
 }
+
+# configuration shortcut {
+conf() {
+	case $1 in
+		xmonad)		vim ~/.xmonad/xmonad.hs ;;
+		tmux)		vim ~/.tmux.conf ;;
+		vim)		vim ~/.vimrc ;;
+		nvim)		vim ~/.config/nvim/init.vim ;;
+		zsh)		vim ~/.zshrc ;;
+		zshut)      vim ~/.zsh/utils.zsh ;;
+		zshal)      vim ~/.zsh/alias.zsh ;;
+		*)		echo "Unknown application $1" ;;
+	esac
+}
+# }
+
+# search the content and open the file
+agopen() {
+    local file_lino="$(ag --filename $1 | fzf | awk -F '[:]' '{print $1, $2}')"
+    if [ ! -z "$file_lino" ]; then
+        local file="$(echo "$file_lino" | awk -F '[  ]' '{print $1}')"
+        local lino="$(echo "$file_lino" | awk -F '[  ]' '{print $2}')"
+        echo -e "+$lino $file" | xargs -o vim
+    fi
+}
+
+# list disk usage. use -r to show descending order
+duls() {
+    local val="$(du -sk * | sort -n $1 | sed -E ':a; s/([[:digit:]]+)([[:digit:]]{3})/\1,\2/; ta')"
+    local val="$(echo $val | head -n 10)"
+    local val="$(echo $val | awk -F'\t' '{printf "%10s %s\n",$1,substr($0,length($1)+2)}')"
+    echo $val
+}
+# }
