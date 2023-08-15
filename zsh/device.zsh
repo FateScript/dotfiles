d10f38c1ef609118a37c29ce2666e78989f1dd1c
Fatescript <wangfeng19950315@163.com>
2023-08-15 19:32:49 +0800

    chore: add device code for zsh

---
+++ zsh/device.zsh
@@ -0,0 +1,28 @@
+
+
+bluetooth_conn()
+{
+    local device_name=$1
+    if [ -z "$device_name" ]; then  # default to the first device
+        device_name=$(bluetooth_name | sed -n '1p')
+    fi
+    # find the bluetooth device address
+    local address=$(blueutil --paired | grep "$device_name" | awk '{print substr($2, 1, length($2) -1)}')
+    echo "connect" "$device_name" "with address: " "$address"
+    blueutil --connect "$address"
+}
+
+bluetooth_name() {
+    # get the paired device information from blueutil
+    paired_devices=$(blueutil --paired)
+    # use awk to extract the names from the output
+    device_names=$(echo "$paired_devices" | awk -F'name: "' '{print $2}' | awk -F'"' '{print $1}')
+    # print the list of device names
+    echo "$device_names"
+}
+
+_bluetooth_device() {
+	choices=`bluetooth_name`
+	suf=( -S '' )
+	_arguments -O suf "*:value:( $choices )"
+}
--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -8,11 +8,13 @@ safe_source $HOME/.zsh/base.zsh
 safe_source $HOME/.zsh/install.zsh
 safe_source $HOME/.zsh/git.zsh
 safe_source $HOME/.zsh/venv.zsh
+safe_source $HOME/.zsh/device.zsh
 
 # compdef
 compdef _src src
 compdef _conf conf
 compdef _act_venv act_venv rm_venv
+compdef _bluetooth_device bluetooth_conn
 
 compdef _git ga=git-add
 compdef _git gc=git-commit
