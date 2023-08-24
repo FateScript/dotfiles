6e388b7eb95205abfe4eee972fa5bfbf377f8dee
Fatescript <wangfeng19950315@163.com>
2023-08-24 21:25:32 +0800

    chore: update macos config and install cmd

--- macos/config_mac.sh
+++ macos/config_mac.sh
@@ -2,3 +2,18 @@
 # vscode
 echo "VSCode: Disable press and hold for keys in favor of key repeat"
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+# to re-enable, execute the following cmd
+# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool true
+
+# iterm2
+echo "iTerm2: Don’t display the annoying prompt when quitting iTerm"
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+# TextEdit
+echo "TextEdit: Set tab width to 4 instead of the default 8"
+defaults write com.apple.TextEdit "TabWidth" '4'
+
+# Safari
+echo "Safari: Configure Google as main search engine"
+defaults write com.apple.Safari SearchProviderIdentifier -string "com.google.Chrome"
+defaults write NSGlobalDomain NSProviderIdentifier -string "com.google.Chrome"
--- macos/install.sh
+++ macos/install.sh
@@ -5,3 +5,16 @@
 brew install coreutils gnu-tar gnu-find
 brew install reattach-to-user-namespace tmux
 brew install wget curl cmake htop the_silver_searcher
+
+# cask app
+brew install --cask iterm2
+brew install --cask visual-studio-code
+brew install --cask google-chrome
+brew install --cask clipy
+brew install --cask clashx
+brew install --cask stats
+brew install --cask iina
+brew install --cask anki
+brew install --cask notion
+brew install --cask eudic
+brew install --cask netnewswire
